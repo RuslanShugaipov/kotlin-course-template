69ebb3dce2d01cf93033941d7d2fc1edc6ba9a51
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2021-10-23 11:02:23 +0300

    First version.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,5 +7,5 @@ fun main() {
 //        println("Enter the correct value!")
 //        lineWidth = readLine()!!.toIntOrNull()
 //    }
-    alignText("TextToAlign3.txt", 1, Alignment.LEFT)
+    alignText("TextToAlign1.txt", 64, Alignment.LEFT)
 }
