b844c26357e3a46d2e8e63c4396ff70310fb3e0e
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2021-10-26 12:07:09 +0300

    Test upgrade

--- src/test/kotlin/alignment/AlignmentKtTest.kt
+++ src/test/kotlin/alignment/AlignmentKtTest.kt
@@ -1,11 +1,10 @@
 package alignment
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
 import java.io.FileNotFoundException
 
-class AlignmentKtTest {
+internal class AlignmentKtTest {
 
     @Test(expected = IllegalArgumentException::class)
     fun `checking for incorrect line width`() {
@@ -14,24 +13,24 @@ class AlignmentKtTest {
 
     @Test
     fun `checking for empty text at the input`() {
-        assertEquals("", alignTextLeft("TextToAlign2.txt", 20))
+        Assert.assertEquals("", alignTextLeft("TextToAlign2.txt", 20))
     }
 
     @Test
     fun `checking alignment without complicated cases`() {
-        assertEquals("C-3PO", alignTextLeft("TextToAlign3.txt", 20))
+        Assert.assertEquals("C-3PO", alignTextLeft("TextToAlign3.txt", 20))
     }
 
     @Test
     fun `checking if the punctuation mark doesn't fit in the line`() {
-        assertEquals("xxxDS\nL. The", alignTextLeft("TextToAlign4.txt", 6))
-        assertEquals("xxxDS\nL. Th\ne", alignTextLeft("TextToAlign4.txt", 5))
+        Assert.assertEquals("xxxDS\nL. The", alignTextLeft("TextToAlign4.txt", 6))
+        Assert.assertEquals("xxxDS\nL. Th\ne", alignTextLeft("TextToAlign4.txt", 5))
     }
 
     @Test
     fun `checking if the line width is 2 and 3`() {
-        assertEquals("xx\nxD\nS\nL.\nTh\ne", alignTextLeft("TextToAlign4.txt", 2))
-        assertEquals("xxx\nDS\nL. \nThe", alignTextLeft("TextToAlign4.txt", 3))
+        Assert.assertEquals("xx\nxD\nS\nL.\nTh\ne", alignTextLeft("TextToAlign4.txt", 2))
+        Assert.assertEquals("xxx\nDS\nL. \nThe", alignTextLeft("TextToAlign4.txt", 3))
     }
 
     @Test(expected = FileNotFoundException::class)
