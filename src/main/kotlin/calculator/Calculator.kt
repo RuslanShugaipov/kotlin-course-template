5866fa183b12779fba7136be3e548397e361f342
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2021-11-03 19:41:51 +0300

    Added functions and checking for incorrect input.

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,11 +1,12 @@
 package calculator
 
 import java.util.*
-import kotlin.math.pow
+import kotlin.math.*
 
+//A class containing operations and returning their priorities
 class OperationPriority {
     companion object {
-        private val Level_5: List<String> = listOf("u-", "u+")
+        private val Level_5: List<String> = listOf("u-", "u+", "sin", "cos", "tg", "ctg", "lg", "ln")
         private val Level_4: List<String> = listOf("^")
         private val Level_3: List<String> = listOf("*", "/")
         private val Level_2: List<String> = listOf("+", "-")
@@ -22,83 +23,138 @@ class OperationPriority {
     }
 }
 
-fun getRPN(expression: String): String{
-    val inputArray = expression.filter { !it.isWhitespace() }.toCharArray()
+//A function that returns the reverse polish notation of the expression
+fun getRPN(expression: String): String?{
+    //The variable that will contain the final expression
     var postfixString = ""
+    try{
+    //Deleting spaces in the expression
+    val inputArray = expression.filter { !it.isWhitespace() }.toCharArray()
+    //Stack for working with operations
     val operationStack: Stack<String> = Stack()
+    var functionName = ""
+    //Variable for checking the unarity of the operation
     var unary = true
     var i = 0
     while(i < inputArray.size) {
-        if(i != 0 && inputArray[i].isDigit() && !postfixString.isEmpty()) postfixString += " "
-        while(inputArray[i].isDigit()) {
-            postfixString += inputArray[i]; i++; if(!operationStack.empty()) if(operationStack.peek() == "(") unary = false; if(i == inputArray.size) break}
-        if(i == inputArray.size) break
-        if(inputArray[i] == '(') {operationStack.push(inputArray[i].toString()); ++i; continue}
-        else if(inputArray[i] == ')') {
-            run loop@ {
+        //If the expression starts with a function or a non-unary operation
+        if (inputArray[0] == ')' || inputArray[0] == '*' || inputArray[0] == '/' || inputArray[0] == '^') throw Exception()
+        //Adding spaces to the output string
+        if (i != 0 && inputArray[i].isDigit() && postfixString.isNotEmpty()) postfixString += " "
+        //Reading a number
+        while (inputArray[i].isDigit()) {
+            postfixString += inputArray[i]; i++; if (!operationStack.empty()) if (operationStack.peek() == "(") unary = false
+            if (i == inputArray.size) break
+        }
+        if (i == inputArray.size) break
+        //Reading a function
+        if (inputArray[i].isLetter()) {
+            while (inputArray[i].isLetter()) {
+                functionName += inputArray[i]; i++; if (i == inputArray.size) break
+            }
+            if(functionName != "sin" && functionName != "cos" && functionName != "tg" && functionName != "ctg" && functionName != "lg" && functionName != "ln")
+                throw Exception()
+            --i
+        }
+        if (i == inputArray.size) break
+        //Just add an opening parenthesis to the stack
+        if (inputArray[i] == '(') { operationStack.push(inputArray[i].toString()); ++i; continue }
+        //If there is a closing parenthesis, then we read operations from the stack to the output
+        //string until we meet an opening parenthesis
+        else if (inputArray[i] == ')') {
+            run loop@{
                 operationStack.withIndex().reversed().forEach { operation ->
-                    if (operation.value == "(") { operationStack.pop(); return@loop }
+                    if(operation.value == "(") { operationStack.pop(); return@loop}
                     else postfixString += " " + operationStack.pop()
                 }
             }
         }
-        else{
-            if(!operationStack.empty()) {
+        else {
+            //If the stack is not empty, then we perform manipulations with the current operation
+            //relative to the operations contained in the stack
+            if (operationStack.isNotEmpty()) {
                 run loop@{
                     operationStack.withIndex().reversed().forEach { operation ->
-                        if(inputArray[i] == '-' || inputArray[i] == '+' && (operationStack.peek() == "-" || operationStack.peek() == "+" || operationStack.peek() == "" || operationStack.peek() == "u-"))
+                        if ((inputArray[i] == '-' || inputArray[i] == '+' || functionName.isNotEmpty()) &&
+                            (operationStack.peek() == "-" || operationStack.peek() == "+" || operationStack.peek() == "" || operationStack.peek() == "u-"))
                             unary = false
-                        if(unary && (inputArray[i] == '-' || inputArray[i] == '+')&& (operationStack.peek() == "(" || operationStack.peek() == "*" || operationStack.peek() == "/"|| operationStack.peek() == "^")){
-                            if(inputArray[i] == '-') operationStack.push("u-")
-                            else operationStack.push("u+")
+                        if (unary && (inputArray[i] == '-' || inputArray[i] == '+' || functionName.isNotEmpty()) && (operationStack.peek() == "(" || operationStack.peek() == "*" || operationStack.peek() == "/" || operationStack.peek() == "^")) {
+                            if (inputArray[i] == '-') operationStack.push("u-")
+                            else if (inputArray[i] == '+') operationStack.push("u+")
+                            else operationStack.push(functionName)
                             unary = false
                             return@loop
                         }
-                        if (OperationPriority.getPriority(operation.value) >= OperationPriority.getPriority(inputArray[i].toString()))
-                            postfixString += " " + operationStack.pop()
-                        else {
-                            operationStack.push(inputArray[i].toString()); return@loop
-                        }
-                        if (operationStack.empty()) {operationStack.push(inputArray[i].toString()); return@loop}
+                        if (OperationPriority.getPriority(operation.value) >= OperationPriority.getPriority(inputArray[i].toString())) { postfixString += " " + operationStack.pop() }
+                        else { operationStack.push(inputArray[i].toString()); return@loop }
+                        if (operationStack.empty()) { operationStack.push(inputArray[i].toString()); return@loop }
                     }
                 }
             }
-            else{
-                if(inputArray[i] == '-') operationStack.push("u-")
-                else if(inputArray[i] == '+') operationStack.push("u+")
+            //If the stack is empty, then either just insert the operation, or check the unarity
+            //of the operation or the presence of a function, then push it
+            else {
+                if (inputArray[i] == '-' && postfixString.isEmpty()) operationStack.push("u-")
+                else if (inputArray[i] == '+' && postfixString.isEmpty()) operationStack.push("u+")
+                else if(functionName.isNotEmpty()) operationStack.push(functionName)
                 else operationStack.push(inputArray[i].toString())
             }
         }
         ++i
         unary = true
+        functionName = ""
     }
-    if(!operationStack.empty()) operationStack.withIndex().reversed().forEach { operation -> postfixString += " " + operationStack.pop()}
+    //All that is left in the stack is added to the output string
+    if(!operationStack.empty()) operationStack.withIndex().reversed().forEach { operation -> postfixString += " " + operation.value; operationStack.pop() }
+    //If the ouputr string contains parentheses, then the expression was incorrect
+    if(postfixString.contains("(") || postfixString.contains(")")) { throw Exception() }
+    }
+    catch(message: Exception){
+        println("The parsing failed! Exception: $message")
+        return null
+    }
     return postfixString
 }
 
-fun calculate(input: String): Float{
+//A function that calculates the value of an expression represented in
+//reverse polish notation
+fun calculate(input: String?): Float?{
     val result: Stack<String> = Stack()
-    val parts = input.split(" ")
-    var leftOperand = 0f
-    var rightOperand: Float
-    var operationResult = 0f
-    for(part in parts){
-        if(part.toIntOrNull() != null) result.push(part)
-        else{
-            rightOperand = result.pop().toFloat()
-            if(part != "u-" && part != "u+")
-                leftOperand = result.pop().toFloat()
-            when(part){
-                "+" -> operationResult = leftOperand + rightOperand
-                "-" -> operationResult = leftOperand - rightOperand
-                "*" -> operationResult = leftOperand * rightOperand
-                "/" -> operationResult = leftOperand / rightOperand
-                "^" -> operationResult = leftOperand.pow(rightOperand)
-                "u-" -> operationResult = rightOperand * (-1)
-                "u+" -> operationResult = rightOperand
+    try {
+        if(input == null)
+            throw Exception()
+        val parts = input.split(" ")
+        var leftOperand = 0f
+        var rightOperand: Float
+        var operationResult = 0f
+        for (part in parts) {
+            if (part.toIntOrNull() != null) result.push(part)
+            else {
+                rightOperand = result.pop().toFloat()
+                if (part != "u-" && part != "u+" && part != "sin" && part != "cos" && part != "tg" && part != "ctg" && part != "ln" && part != "lg")
+                    leftOperand = result.pop().toFloat()
+                when (part) {
+                    "+" -> operationResult = leftOperand + rightOperand
+                    "-" -> operationResult = leftOperand - rightOperand
+                    "*" -> operationResult = leftOperand * rightOperand
+                    "/" -> operationResult = leftOperand / rightOperand
+                    "^" -> operationResult = leftOperand.pow(rightOperand)
+                    "u-" -> operationResult = rightOperand * (-1)
+                    "u+" -> operationResult = rightOperand
+                    "sin" -> operationResult = sin(rightOperand)
+                    "cos" -> operationResult = cos(rightOperand)
+                    "tg" -> operationResult = tan(rightOperand)
+                    "ctg" -> operationResult = atan(rightOperand)
+                    "ln" -> operationResult = ln(rightOperand)
+                    "lg" -> operationResult = log(rightOperand, 10f)
+                }
+                result.push(operationResult.toString())
             }
-            result.push(operationResult.toString())
         }
+    }
+    catch(message: Exception){
+        println("The parsing failed! Exception: $message")
+        return null
     }
     return result.pop().toFloat()
 }
