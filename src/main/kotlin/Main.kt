9b15cf3e4c52a726e80a271d61911d6973175a34
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2021-11-03 19:42:55 +0300

    Added some code in main for lab2.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
-import alignment.*
+//import alignment.*
 import calculator.*
 
 fun main() {
+    //Code for the alignment (lab 1)
 //    println("Enter the line width: ")
 //    var lineWidth: Int? = readLine()!!.toIntOrNull()
 //    while(lineWidth == null){
@@ -10,10 +11,6 @@ fun main() {
 //    }
 //    print(alignText("TextToAlign5.txt", 0, Alignment.LEFT))
 
-
-    val input1 = "1+(4-2)*6"
-    val input2 = "( 3+ (4*2)/  ( ((1-5))^ 2))"
-    val output = getRPN(input2)
-    println(input2+"\n"+output)
-    print("Result: ${calculate(output)}")
+    //Code for the calculator (lab 2)
+    print("Result: ${calculate(getRPN("sin(5)"))}")
 }
