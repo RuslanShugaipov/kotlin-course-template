4d6f769d463b92de81c5f33184d52fdb0d171d09
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2021-11-21 00:35:05 +0300

    Fixed lab 3.

--- src/main/kotlin/shape/Shape.kt
+++ src/main/kotlin/shape/Shape.kt
@@ -7,19 +7,14 @@ import kotlin.random.Random
 interface Shape {
     fun calcArea(): Double
     fun calcPerimeter(): Double
-    fun getShapeName(): String
 }
 
 class Circle(
-    _radius: Double
+    val radius: Double
 ) : Shape {
-    private val radius: Double
-
     init {
-        if (_radius <= 0) {
+        if (radius <= 0) {
             error("Incorrect radius value!")
-        } else {
-            radius = _radius
         }
     }
 
@@ -30,22 +25,14 @@ class Circle(
     override fun calcPerimeter(): Double {
         return 2 * PI * radius
     }
-
-    override fun getShapeName(): String {
-        return "Cirlce"
-    }
 }
 
 class Square(
-    _side: Double
+    val side: Double
 ) : Shape {
-    private val side: Double
-
     init {
-        if (_side <= 0) {
+        if (side <= 0) {
             error("Incorrect side value!")
-        } else {
-            side = _side
         }
     }
 
@@ -56,29 +43,18 @@ class Square(
     override fun calcPerimeter(): Double {
         return 4 * side
     }
-
-    override fun getShapeName(): String {
-        return "Square"
-    }
 }
 
 class Rectangle(
-    _length: Double,
-    _width: Double
+    val length: Double,
+    val width: Double
 ) : Shape {
-    private val length: Double
-    private val width: Double
-
     init {
-        if (_length <= 0) {
+        if (length <= 0) {
             error("Incorrect length value!")
-        } else {
-            length = _length
         }
-        if (_width <= 0) {
+        if (width <= 0) {
             error("Incorrect width value!")
-        } else {
-            width = _width
         }
     }
 
@@ -89,28 +65,16 @@ class Rectangle(
     override fun calcPerimeter(): Double {
         return 2 * (length + width)
     }
-
-    override fun getShapeName(): String {
-        return "Rectangle"
-    }
 }
 
 class Triangle(
-    _sideA: Double,
-    _sideB: Double,
-    _sideC: Double
+    val sideA: Double,
+    val sideB: Double,
+    val sideC: Double
 ) : Shape {
-    private val sideA: Double
-    private val sideB: Double
-    private val sideC: Double
-
     init {
-        if (_sideA <= 0 || _sideB <= 0 || _sideC <= 0 || ((_sideA + _sideB) < _sideC || (_sideA + _sideC) < _sideB || (_sideB + _sideC) < _sideA)) {
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0 || ((sideA + sideB) < sideC || (sideA + sideC) < sideB || (sideB + sideC) < sideA)) {
             error("Incorrect side value!")
-        } else {
-            sideA = _sideA
-            sideB = _sideB
-            sideC = _sideC
         }
     }
 
@@ -121,10 +85,6 @@ class Triangle(
 
     override fun calcPerimeter(): Double {
         return sideA + sideB + sideC
-    }
-
-    override fun getShapeName(): String {
-        return "Triangle"
     }
 }
 
@@ -160,58 +120,27 @@ class ShapeFactorImpl : ShapeFactory {
     }
 
     override fun createRandomCircle(): Circle {
-        var randomRadius: Double = -1.0
-        while (randomRadius <= 0) {
-            randomRadius = Random.nextDouble()
-        }
-        return Circle(randomRadius)
+        return Circle(Random.nextDouble(0.0, Double.MAX_VALUE))
     }
 
     override fun createRandomSquare(): Square {
-        var randomSide: Double = -1.0
-        while (randomSide <= 0) {
-            randomSide = Random.nextDouble()
-        }
-        return Square(randomSide)
+        return Square(Random.nextDouble(0.0, Double.MAX_VALUE))
     }
 
     override fun createRandomRectangle(): Rectangle {
-        var randomLength: Double = -1.0
-        var randomWidth: Double = -1.0
-        while (randomLength <= 0) {
-            randomLength = Random.nextDouble()
-        }
-        while (randomWidth <= 0) {
-            randomWidth = Random.nextDouble()
-        }
-        return Rectangle(randomLength, randomWidth)
+        return Rectangle(Random.nextDouble(0.0, Double.MAX_VALUE), Random.nextDouble(0.0, Double.MAX_VALUE))
     }
 
     override fun createRandomTriangle(): Triangle {
-        var randomSideA: Double = -1.0
-        var randomSideB: Double = -1.0
-        var randomSideC: Double = -1.0
-        while ((randomSideA + randomSideB) < randomSideC || (randomSideA + randomSideC) < randomSideB || (randomSideB + randomSideC) < randomSideA) {
-            while (randomSideA <= 0) {
-                randomSideA = Random.nextDouble()
-            }
-            while (randomSideB <= 0) {
-                randomSideB = Random.nextDouble()
-            }
-            while (randomSideC <= 0) {
-                randomSideC = Random.nextDouble()
-            }
-        }
-        return Triangle(randomSideA, randomSideB, randomSideC)
+        return Triangle(Random.nextDouble(0.0, Double.MAX_VALUE), Random.nextDouble(0.0, Double.MAX_VALUE), Random.nextDouble(0.0, Double.MAX_VALUE))
     }
 
     override fun createRandomShape(): Shape {
-        when (Random.nextInt(1, 4)) {
-            1 -> return createRandomCircle()
-            2 -> return createRandomSquare()
-            3 -> return createRandomRectangle()
-            4 -> return createRandomTriangle()
-            else -> return createRandomShape()
+        return when (Random.nextInt(1, 5)) {
+            1 -> createRandomCircle()
+            2 -> createRandomSquare()
+            3 -> createRandomRectangle()
+            else -> createRandomTriangle()
         }
     }
 }
